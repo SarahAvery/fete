db5739e10b93f9e362b0769bb6c179b626a23ca8
fictionalparakeets <banksmatthew32@gmail.com>
2021-09-03 22:03:20 -0400

    update with working open and close for form

--- src/components/Dashboard/ModifyEventForm.jsx
+++ src/components/Dashboard/ModifyEventForm.jsx
@@ -7,29 +7,31 @@ import "react-datepicker/dist/react-datepicker.css";
 
 const ModifyEventForm = (props) => {
   const { updateEvent, deleteEvent } = useDashboard();
+  const { closeForm } = props
   const event = props.event;
-  const closeForm = props.close;
+  const formatDate = props.dateFormat;
+  const formatPhone = props.phoneFormat;
 
   // Format phone number when it's returned to the form for editing
-  const formatPhoneState = (phone) => {
-    return phone.includes('-') ? phone : `${phone.slice(0, 3)}-${phone.slice(3, 6)}-${phone.slice(6, 10)}`
-  };
+  // const formatPhoneState = (phone) => {
+  //   return phone.includes('-') ? phone : `${phone.slice(0, 3)}-${phone.slice(3, 6)}-${phone.slice(6, 10)}`
+  // };
 
   // Format the date so the datepicker can re-open with that selected
   // This should be improved and moved to a separate func
-  const formatDateOutput = (timestamptz) => {
-    // console.log('date (timestamptz): ', timestamptz) // => 2016-06-23T02:10:25.000Z
-    const formatted = timestamptz.slice(0, 10);
-    // console.log('formatted: ', formatted) // => 2016-06-23
-    return formatted;
-  };
+  // const formatDateOutput = (timestamptz) => {
+  //   // console.log('date (timestamptz): ', timestamptz) // => 2016-06-23T02:10:25.000Z
+  //   const formatted = timestamptz.slice(0, 10);
+  //   // console.log('formatted: ', formatted) // => 2016-06-23
+  //   return formatted;
+  // };
 
   // state for form entry fields
   const [eventTitle, setEventTitle] = useState(event.title);
   const [firstName, setFirstName] = useState(event.first_name);
   const [secondName, setSecondName] = useState(event.second_name);
   const [email, setEmail] = useState(event.email);
-  const [phone, setPhone] = useState(formatPhoneState(event.phone));
+  const [phone, setPhone] = useState(formatPhone(event.phone));
   const [unit, setUnit] = useState(event.unit);
   const [streetNo, setStreetNo] = useState(event.street_number);
   const [streetName, setStreetName] = useState(event.street_name);
@@ -207,7 +209,7 @@ const ModifyEventForm = (props) => {
           <div className="datepicker container">
             <div className="display-event-date">
               <p>Current Event Date:</p>
-              {!newDate && <p>{() => formatDateOutput(date)}</p>}
+              {!newDate && <p>{formatDate(date)}</p>}
             </div>
 
             <label htmlFor="date">Change Event Date:</label>

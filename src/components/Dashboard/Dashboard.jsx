4c944daa73f7c2c76ba37ff5e355c1fd20df3d9a
fictionalparakeets <68305611+fictionalparakeets@users.noreply.github.com>
2021-09-04 12:59:54 -0400

    fix dashboard render on events update (#58)

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import DashboardItem from "./DashboardItem";
 import { useDashboard, withDashboard } from "../../contexts/DashboardContext";
 import NewEvent from "./NewEvent";
 import Modal from "../Modal";
 
-// import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const Dashboard = () => {
   const [form, setForm] = useState({ visible: false });
-  const { events, getEvents } = useDashboard();
+  const { events } = useDashboard();
 
   const openForm = () => {
     setForm({ visible: true });
@@ -19,10 +18,6 @@ const Dashboard = () => {
   const closeForm = () => {
     setForm({ visible: false });
   };
-  
-  // useEffect(() => {
-  //   getEvents()
-  // }, [events]);
 
   return (
     <div className="Dashboard ">
@@ -35,18 +30,24 @@ const Dashboard = () => {
             </button>
           </div>
         </div>
-
         {form && (
           <div>
-            <Modal isOpen={form.visible} onClose={() => setForm({ ...form, visible: false })}>
+            <Modal 
+              isOpen={form.visible} 
+              onClose={() => 
+              setForm({ ...form, visible: false })}>
               <NewEvent closeForm={closeForm} />
             </Modal>
           </div>
         )}
-
         <ul>
           {events?.map((event) => (
-            <DashboardItem key={event.event_id} {...event} closeForm={closeForm} openForm={openForm} />
+            <DashboardItem 
+              key={event.event_id} 
+              {...event} 
+              closeForm={closeForm} 
+              openForm={openForm} 
+            />
           ))}
         </ul>
       </div>
@@ -56,8 +57,7 @@ const Dashboard = () => {
 
 // Dashboard.propTypes = {
 //   events: PropTypes.array,
-
 //   key: PropTypes.number,
-
 // };
+
 export default withDashboard(Dashboard);

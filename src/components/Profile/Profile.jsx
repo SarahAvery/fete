e6b7ce81b13baaa8b7dfdc7e810a24c356a0d6b5
fictionalparakeets <banksmatthew32@gmail.com>
2021-09-06 16:25:16 -0400

    clean up percentage setting

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,8 +4,6 @@ import Modal from "../Modal";
 import ModifyEventForm from "../Dashboard/ModifyEventForm";
 import { useProfile, withProfile } from "../../contexts/ProfileContext";
 
-
-
 import Pie from "../Dashboard/Circle";
 import "../Dashboard/Dashboard.scss";
 import { Link } from "react-router-dom";
@@ -38,10 +36,10 @@ const Profile = () => {
 
   useEffect(() => {
     if (event) {
-      setCompletionPercent(Math.round((parseInt(event.completed_tasks) / parseInt(event.total_tasks)) * 100))
-      setBudgetPercent(Math.round((parseInt(event.expense_actual) / parseInt(event.expense_budget)) * 100))
+      setCompletionPercent(Math.round((parseInt(event.completed_tasks) / parseInt(event.total_tasks)) * 100));
+      setBudgetPercent(Math.round((parseInt(event.expense_actual) / parseInt(event.expense_budget)) * 100));
     }
-  }, [event])
+  }, [event]);
 
   const openForm = () => {
     setIsOpen({ visible: true });
@@ -61,24 +59,26 @@ const Profile = () => {
   };
 
   const formatMoneyOutput = (money) => {
-    return `$${money.toLocaleString("en-US")}`
-  }
+    return `$${money.toLocaleString("en-US")}`;
+  };
 
   return (
     <Fragment>
       <li className="DashbordItem">
         <div className="DashboardItem__content">
           <div className="DashboardItem__titleBox">
-            { event && <h3>{event.title}</h3> }
+            {event && <h3>{event.title}</h3>}
             <i className="fas fa-ellipsis-h" onClick={() => openForm()}></i>
             <ModifyModal isOpen={isOpen.visible} onClose={() => setIsOpen({ ...isOpen, visible: false })}>
-              { event && <ModifyEventForm
-                event={event}
-                closeForm={closeForm}
-                openForm={openForm}
-                dateFormat={formatDateOutput}
-                phoneFormat={formatPhoneState}
-              /> }
+              {event && (
+                <ModifyEventForm
+                  event={event}
+                  closeForm={closeForm}
+                  openForm={openForm}
+                  dateFormat={formatDateOutput}
+                  phoneFormat={formatPhoneState}
+                />
+              )}
             </ModifyModal>
           </div>
           <div className="DashboardItems__container">
@@ -86,28 +86,32 @@ const Profile = () => {
               <h3>Contact Info</h3>
               <div className="info-section">
                 <p className="subtitle">Names:</p>
-                { event && <p>
-                   {event.first_name} & {event.second_name}
-                </p> }
+                {event && (
+                  <p>
+                    {event.first_name} & {event.second_name}
+                  </p>
+                )}
               </div>
               <div className="info-section">
                 <p className="subtitle">Date:</p>
-                { event && <p>{formatDateOutput(event.event_date)}</p> }
+                {event && <p>{formatDateOutput(event.event_date)}</p>}
               </div>
               <div className="info-section">
                 <p className="subtitle">Email:</p>
-                { event && <p>{event.email}</p> }
+                {event && <p>{event.email}</p>}
               </div>
               <div className="info-section">
                 <p className="subtitle">Phone:</p>
-                { event && <p>{formatPhoneState(event.phone)}</p> }
+                {event && <p>{formatPhoneState(event.phone)}</p>}
               </div>
               <div className="info-section">
                 <p className="subtitle">Address:</p>
-                { event && <p>
-                  {event.unit} {event.street_number}, {event.street_name} {event.street_type}, {event.postal_code},{" "}
-                  {event.city}
-                </p> }
+                {event && (
+                  <p>
+                    {event.unit} {event.street_number}, {event.street_name} {event.street_type}, {event.postal_code},{" "}
+                    {event.city}
+                  </p>
+                )}
               </div>
             </div>
             <h3>Progress: </h3>
@@ -116,23 +120,23 @@ const Profile = () => {
             </div>
             <h3>Budget: </h3>
             <div className="DashboardItems__progress">
-              { event && <p>Event Budget: {formatMoneyOutput(event.expense_budget)}</p> }
-              { event && <p>Item Expense Total: {formatMoneyOutput(event.expense_actual)}</p> }
-              { event && <Pie className="pie" percentage={budgetPercent} colour="rgb(130, 156, 167)" /> }
+              {event && <p>Event Budget: {formatMoneyOutput(event.expense_budget)}</p>}
+              {event && <p>Item Expense Total: {formatMoneyOutput(event.expense_actual)}</p>}
+              {event && <Pie className="pie" percentage={budgetPercent} colour="rgb(130, 156, 167)" />}
             </div>
           </div>
           <div className="btn-container-parent">
-          <div className="btn-container">
-            <Link className="button" to={`/board?eventId=${eventId}`}>
-              View Board
-            </Link>
-          </div>
+            <div className="btn-container">
+              <Link className="button" to={`/board?eventId=${eventId}`}>
+                View Board
+              </Link>
+            </div>
           </div>
         </div>
       </li>
     </Fragment>
   );
-}
+};
 
 // Profile.propTypes = {
 

014d46aa05301d4975b47b4f035c64faf70e4b59
fictionalparakeets <banksmatthew32@gmail.com>
2021-09-07 00:51:44 -0400

    add api req for single event

--- src/contexts/DashboardContext.jsx
+++ src/contexts/DashboardContext.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { apiRequest } from "../../src/utils/apiUtils";
 
 export const DashboardContext = React.createContext();
@@ -6,15 +6,30 @@ export const DashboardContext = React.createContext();
 const DashboardContextProvider = ({ children }) => {
   const [events, setEvents] = useState();
   const [data, setDashboardData] = useState();
+  const [event, setSingleEvent] = useState();
+  const [profileData, setProfileData] = useState();
 
-  useEffect(() => {
-    getEvents();
-  }, [data]);
 
   const getEvents = () => {
     apiRequest(`${process.env.REACT_APP_API_URL}/events`, { method: "GET" })
       .then((res) => res.json())
       .then((data) => setEvents(data));
+  };
+
+  const getEvent = async () => {
+    const params = new URLSearchParams(document.location.search.substring(1));
+    const eventId = params.get("eventId");
+
+    apiRequest(`${process.env.REACT_APP_API_URL}/events`, { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        data.forEach(event => {
+          if (String(event.event_id) === eventId) {
+            setSingleEvent(event)
+            return event
+          }
+        })
+      });
   };
 
   const addEvent = async (formData, user) => {
@@ -28,6 +43,7 @@ const DashboardContextProvider = ({ children }) => {
         body: JSON.stringify({ ...formData, userId }),
       });
       setDashboardData(formData);
+      setProfileData(formData)
     } catch {
       console.log("ERROR in addEvent function inside NewEvent component");
     }
@@ -41,6 +57,8 @@ const DashboardContextProvider = ({ children }) => {
         headers: { "Content-Type": "application/json" },
       });
       setDashboardData(data);
+      setProfileData(data)
+
     } catch (err) {
       console.log(err);
     }
@@ -54,13 +72,15 @@ const DashboardContextProvider = ({ children }) => {
         headers: { "Content-Type": "application/json" },
       });
       setDashboardData(data);
+      setProfileData(data)
+
     } catch (err) {
       console.log(err);
     }
   };
 
   return (
-    <DashboardContext.Provider value={{ events, getEvents, setEvents, addEvent, updateEvent, deleteEvent }}>
+    <DashboardContext.Provider value={{ data, profileData, event, getEvent, events, getEvents, setEvents, addEvent, updateEvent, deleteEvent }}>
       {children}
     </DashboardContext.Provider>
   );
